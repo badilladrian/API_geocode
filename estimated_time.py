80ed72cb8ceedbcb608e25274dbfafffb0c868bd
Adrian Badilla <badilladrianch@gmail.com>
2021-04-05 08:38:45 -0600

    init

---
+++ estimated_time.py
@@ -0,0 +1,19 @@
+import requests, json
+  
+api_key ='AIzaSyC9_hj9Dx9xRtLls8JXDTO82MY6Pf0FNfI'
+  
+source = "'lat': 39.7612992, 'lon': -86.1519681"
+  
+dest = "'lat': 39.7622292, 'lon': -86.1578917"
+  
+url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+  
+# return response object
+result = requests.get(url + 'origins = ' + source +
+                   '&destinations = ' + dest +
+                   '&key = ' + api_key)
+                     
+# return json format result
+eta_time = result.json()
+    
+print(eta_time)
