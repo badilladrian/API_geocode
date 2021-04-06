fb4d0f958efaaa889fe39a1a32d6cf4b1d9ffb98
alizano94 <lizanoandresfco@gmail.com>
2021-04-05 22:28:59 -0500

    Secure Keys

--- API.py
+++ API.py
@@ -5,8 +5,6 @@ from datetime import datetime
 credentials = yaml.load(open('./gmaps.yaml'))
 url = credentials['database']['url']
 api_key = credentials['database']['GOOGLE_API_KEY']
-print("URL: " +url)
-print("Key: " +api_key)
 
 
 #Functions
